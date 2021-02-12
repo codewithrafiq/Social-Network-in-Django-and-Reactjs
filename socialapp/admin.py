0b0e879ab31dacc1f57ae3c0d5ddcfbc09a6eb5b
rafiquljislam <rafiquljislam@gmail.com>
2021-02-12 21:21:51 +0600

    part-2

--- socialapp/admin.py
+++ socialapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import (
+    Profile,
+    Post,
+    Comment,
+    Reply,
+    Like,
+    FriendRequest,
+)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Reply)
+admin.site.register(Like)
+admin.site.register(FriendRequest)
--- socialapp/models.py
+++ socialapp/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    image = models.ImageField(default='image/profile.png', upload_to='image/')
+    friends = models.ManyToManyField(User, related_name='friends', blank=True)
+    time = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, *args, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        Token.objects.create(user=instance)
+
+
+class Post(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    time = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Like(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+
+
+class Comment(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    title = models.TextField()
+    time = models.DateField(auto_now_add=True)
+
+
+class Reply(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    title = models.TextField()
+    time = models.DateField(auto_now_add=True)
+
+
+class FriendRequest(models.Model):
+    sender = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='receiver')
+    time = models.DateField(auto_now_add=True)
